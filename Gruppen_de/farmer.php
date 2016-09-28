bc3e547ed6bc9d82eb28da198bd4b36356812cfe
TheCanadians <vschnoor@mc-kiel.de>
2016-09-28 23:23:48 +0200

    Gruppen erstellt
    - Gruppen nach strukt.html erstellt

---
+++ Gruppen_de/farmer.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<form action=gameHandler.php method=post>
+	<input type=image src=# alt=Submit id=Ware_01>
+	<input type=image src=# alt=Submit id=Ware_02>
+</form>
+</body>
+</html>
---
+++ Gruppen_de/plants.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+	<?php include 'babylon.php'; ?>
+	<button type=submit formaction=gameHandler.php formmethod=post name=levelUpPflanze value=1>
+</body>
+</html>
